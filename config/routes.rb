c6bbec4bcc6745881cb794b06a77193d930a7786
Denis Neverov <denis.neverov@gmail.com>
2020-06-15 03:09:36 +0300

    Add scopes to Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   resources :episodes do
     patch :toggle_completed, on: :member
+    collection do
+      get 'completed', to: "episodes#index", scope: "completed"
+      get 'uncompleted', to: "episodes#index", scope: "uncompleted"
+    end
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
